a5790c9dbb3c85ecf00c6f2349aa045cfb2ec072
Michael Gebregiorgis <you@example.com>
2023-12-14 07:20:56 -0800

    Initial file

---
+++ main.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+int flag = 0;
+/**
+ * main - monty program
+ *
+ * @ac: Data type
+ *
+ * @av: Data type
+ *
+ * Return: 0
+ */
+int main(int ac, char **av)
+{
+	stack_t *stk;
+
+	stk = NULL;
+	if (ac != 2)
+	{
+		printf("USAGE: MONTY FILE\n");
+		error_exit(&stk);
+	}
+	file_read(av[1], &stk);
+	free_int(stk);
+	return (0);
+}
